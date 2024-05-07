2b7e0c30d9872ada7c0d2006dc1472fdcf266ab1
MuselianiMariami <museliani61@gmail.com>
2024-05-07 18:21:30 +0400

    Initial Commit

---
+++ TTT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTT
+//
+//  Created by Mariam Museliani on 07.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
